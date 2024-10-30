6370f9547643fb4c75187b27f5ed8a3f68b195c2
Rafael de Souza Silva <77814194+RafaSS@users.noreply.github.com>
2024-10-30 16:14:29 -0300

    tests

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"com.example.demo.controller", "com.example.demo.servico", "com.example.demo.repository", "com.example.demo.exception", "com.example.demo.modelo", "com.example.demo.exception"})
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.demo.controller", "com.example.demo.servico", "com.example.demo.repository", "com.example.demo.exception", "com.example.demo.modelo", "com.example.demo.exception"})
 public class DemoApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,14 +1,16 @@
-//package com.example.demo;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class DemoApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//
-//    }
-//
-//}
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = {"com.example.demo.controller", "com.example.demo.servico", "com.example.demo.repository", "com.example.demo.exception", "com.example.demo.modelo", "com.example.demo.exception"})
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
